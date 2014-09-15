444467f7f63da0fa06dbb21e3335468bd0a7120c
zabkasirna <zabka.sirna.gwen@gmail.com>
2014-09-16 03:11:20 +0700

    Thumb slider, DONE.

--- client/scripts/module/ui/ui-thumb.js
+++ client/scripts/module/ui/ui-thumb.js
@@ -2,10 +2,11 @@ module.exports = {
     element: $(),
     init: function(el) {
         this.element = el;
-        var self         = this
-        ,   $thumbGroups = self.element.find('.thumb-groups')
-        ,   $thumbs      = self.element.find('.thumb')
-        ,   isSwap       = false
+        var self           = this
+        ,   $thumbGroups   = self.element.find('.thumb-groups')
+        ,   $thumbs        = self.element.find('.thumb')
+        ,   $pageSelectors = $('.page-selectors')
+        ,   isSwap         = false
         ;
 
         var _thumbN = $thumbs.length
@@ -14,20 +15,22 @@ module.exports = {
         ,   _thumbStart = 0
         ,   _thumbEnd = _thumbGroupN
         ,   _thumbGroupTemplate = '<div class="thumb-group" />'
+        ,   _elWidth = this.element.width()
+        ,   _thumbGroupLeft = 0
         ;
 
         // dynamically set .thumb-groups width
         $thumbGroups.css('width', 100 * _thumbGroupN + "%");
 
+        // dynamically group .thumb, and create page-selector
         for (var i = 0; i < _thumbGroupN; i ++) {
+            // begin grouping thumb
             _thumbStart = ((_thumbGroupsLimit + 0) * i);
             _thumbEnd = _thumbStart + _thumbGroupsLimit;
             $thumbs
                 .slice( _thumbStart, _thumbEnd )
                 .wrapAll(_thumbGroupTemplate)
                 ;
-
-            console.log('_thumbGroupN:', _thumbGroupN, '_thumbStart:', _thumbStart, '_thumbEnd:', _thumbEnd);
         }
 
         // Randomize thumb image/text position
@@ -36,5 +39,61 @@ module.exports = {
             isSwap = Math.random() >= 0.5 ? isSwap : !isSwap;
             if (isSwap) $(this).addClass('is-swap');
         });
+
+        // dynamically create page-selector
+        var _pageSelectorPrevTemplate = '<li class="page-selector prev"><a data-slide class="page-selector-link" href="javascript:void(0)"><object data="assets/images/icon/arrow-black.svg" type="image/svg+xml" class="icon">&lt;</object></a></li>'
+        ,   _pageSelectorNumTemplate  = '<li class="page-selector"><a data-slide class="page-selector-link recolor" href="javascript:void(0)"></a></li>'
+        ,   _pageSelectorNextTemplate = '<li class="page-selector next"><a data-slide class="page-selector-link" href="javascript:void(0)"><object data="assets/images/icon/arrow-black.svg" type="image/svg+xml" class="icon">&gt;</object></a></li>'
+        ;
+        var $selector = $()
+        ,   _dataSlide = 0
+        ;
+
+        // First: create page-selector prev
+        if (_thumbGroupN > 1) $pageSelectors.append(_pageSelectorPrevTemplate);
+        $('.page-selector.prev').find('.page-selector-link').attr('data-slide', 'prev');
+
+        // Next: create page-selector num
+        for (var j = 0; j < _thumbGroupN; j ++) {
+            $pageSelectors.append(_pageSelectorNumTemplate);
+        }
+        $('.page-selector:not(.prev)').find('.page-selector-link').each(function(index) {
+            $(this)
+                .text((index + 1))
+                .attr('data-slide', index)
+                ;
+        });
+
+        // Default style for page-selector-link
+        if (_thumbGroupN > 1) $('.page-selector:not(.prev)').eq(0).find('.page-selector-link').addClass('active');
+        else $('.page-selector:not(.prev)').eq(0).find('.page-selector-link').css('display', 'none');
+
+        // Last: create page-selector next
+        if (_thumbGroupN > 1) $pageSelectors.append(_pageSelectorNextTemplate);
+        $('.page-selector.next').find('.page-selector-link').attr('data-slide', 'next');
+
+        // Add event to page-selector
+        $('.page-selector').each(function() {
+            $selector = $(this).find('.page-selector-link');
+            $selector.on('click', function(e) {
+                e.preventDefault();
+
+                if ($(this).data('slide') === 'prev') _dataSlide --;
+                else if ($(this).data('slide') === 'next') _dataSlide ++;
+                else _dataSlide = $(this).data('slide');
+
+                if (_dataSlide < 0) _dataSlide = 0;
+                else if (_dataSlide > (_thumbGroupN - 1)) _dataSlide = (_thumbGroupN - 1);
+
+                $thumbGroups.attr('data-slide', _dataSlide);
+                _thumbGroupLeft = $thumbGroups.attr('data-slide') * _elWidth;
+                $thumbGroups.css( 'left', '-' + _thumbGroupLeft + 'px' );
+
+                $('.page-selector-link').removeClass('active');
+                var _linkSelectorString = '.page-selector-link[data-slide="' + _dataSlide + '"]';
+                $(_linkSelectorString).addClass('active');
+            });
+        })
+        ;
     }
 };
