17a8a21038c05ff76549715e1f453b19e9930def
zabkasirna <zabka.sirna.gwen@gmail.com>
2014-09-15 21:23:00 +0700

    Prepare for thumb slider. Remove page-selector-link by default.

--- client/scripts/module/ui/ui-thumb.js
+++ client/scripts/module/ui/ui-thumb.js
@@ -2,17 +2,26 @@ module.exports = {
     element: $(),
     init: function(el) {
         this.element = el;
-        var self = this
-        ,   $thumbs = self.element.find('.thumb')
-        ,   isSwap = false
+        var self         = this
+        ,   $thumbGroups = self.element.find('.thumb-groups')
+        // ,   $thumbGroup  = self.element.find('.thumb-group')
+        // ,   $thumbGroupN = $thumbGroup.length
+        ,   $thumbs      = self.element.find('.thumb')
+        // ,   $selectors   = $('.page-selector-link')
+        ,   isSwap       = false
         ;
-
-        console.log('element:', $thumbs);
 
         $thumbs.each(function(i) {
             $(this).removeClass('is-swap');
             isSwap = Math.random() >= 0.5 ? isSwap : !isSwap;
             if (isSwap) $(this).addClass('is-swap');
         });
+
+        var $thumbN = $thumbs.length
+        ,   $thumbGroupsLimit = 6
+        ,   $thumbGroupN = ($thumbN - ($thumbN % $thumbGroupsLimit)) / $thumbGroupsLimit;
+        ;
+
+        console.log('thumbGroupN:', $thumbGroupN);
     }
 };
