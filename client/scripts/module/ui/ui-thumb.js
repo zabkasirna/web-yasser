617c0ad0a9d732663b2454eb2097e46e71e07fa7
zabkasirna <zabka.sirna.gwen@gmail.com>
2014-09-15 23:00:40 +0700

    Dynamically groups thumb, based on thumbs length.

--- client/scripts/module/ui/ui-thumb.js
+++ client/scripts/module/ui/ui-thumb.js
@@ -4,24 +4,37 @@ module.exports = {
         this.element = el;
         var self         = this
         ,   $thumbGroups = self.element.find('.thumb-groups')
-        // ,   $thumbGroup  = self.element.find('.thumb-group')
-        // ,   $thumbGroupN = $thumbGroup.length
         ,   $thumbs      = self.element.find('.thumb')
-        // ,   $selectors   = $('.page-selector-link')
         ,   isSwap       = false
         ;
 
+        var _thumbN = $thumbs.length
+        ,   _thumbGroupsLimit = 6
+        ,   _thumbGroupN = (_thumbN > _thumbGroupsLimit) ? Math.ceil(_thumbN / _thumbGroupsLimit) : 1
+        ,   _thumbStart = 0
+        ,   _thumbEnd = _thumbGroupN
+        ,   _thumbGroupTemplate = '<div class="thumb-group" />'
+        ;
+
+        // dynamically set .thumb-groups width
+        $thumbGroups.css('width', 100 * _thumbGroupN + "%");
+
+        for (var i = 0; i < _thumbGroupN; i ++) {
+            _thumbStart = ((_thumbGroupsLimit + 0) * i);
+            _thumbEnd = _thumbStart + _thumbGroupsLimit;
+            $thumbs
+                .slice( _thumbStart, _thumbEnd )
+                .wrapAll(_thumbGroupTemplate)
+                ;
+
+            console.log('_thumbGroupN:', _thumbGroupN, '_thumbStart:', _thumbStart, '_thumbEnd:', _thumbEnd);
+        }
+
+        // Randomize thumb image/text position
         $thumbs.each(function(i) {
             $(this).removeClass('is-swap');
             isSwap = Math.random() >= 0.5 ? isSwap : !isSwap;
             if (isSwap) $(this).addClass('is-swap');
         });
-
-        var $thumbN = $thumbs.length
-        ,   $thumbGroupsLimit = 6
-        ,   $thumbGroupN = ($thumbN - ($thumbN % $thumbGroupsLimit)) / $thumbGroupsLimit;
-        ;
-
-        console.log('thumbGroupN:', $thumbGroupN);
     }
 };
