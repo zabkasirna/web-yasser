a073be5ad462004723606cd66c270a4bc1539253
zabkasirna <zabka.sirna.gwen@gmail.com>
2014-09-17 04:00:12 +0700

    Page subnavigation done.

--- client/scripts/module/ui/ui-navigation.js
+++ client/scripts/module/ui/ui-navigation.js
@@ -19,12 +19,24 @@ module.exports = {
     initPageNavigation:  function(hasInfo) {
         var $infoBtnTogglers = $('.info-btn-togglers')
         ,   $pageInfos = $('.page-infos')
+        ,   $pageSubNavs = $('#page-subnavigations')
+        ,   $pageSubNavLink = $('#page-subnavigation-link')
         ;
 
         $infoBtnTogglers.on('click', function() {
             $(this).toggleClass('is-active');
-            $pageInfos.toggleClass('is-active');
-            console.log($pageInfos);
+            if($pageInfos.length) $pageInfos.toggleClass('is-active');
+            if($pageSubNavs.length) $pageSubNavs.toggleClass('is-active');
+        });
+
+        $pageSubNavs.find('.page-subnavigation-link').each(function() {
+            $(this).on('mouseenter', function(e) {
+                $infoBtnTogglers
+                    .find('.info-btn-toggler[data-project]')
+                    .attr('data-project', $(this).data('project'))
+                    .text($(this).data('project'))
+                    ;
+            });
         });
     }
 };
